5e95584845ce69011c12eb6122b0daec32353a78
bfalecki <01158532@pw.edu.pl>
2026-01-17 22:37:29 +0100

    phase discontinuities removal

--- addPaths.m
+++ addPaths.m
@@ -1,2 +1,5 @@
 addpath(genpath("C:\Users\bfalecki\Documents\challenge\Healthcare-Radar-Project\"))
 addpath(genpath("C:\Users\bfalecki\Documents\challenge\Phaser-Control-with-MATLAB\"))
+
+addpath(genpath("/home/user/Documents/radarConfChallenge/Healthcare-Radar-Project/"))
+addpath(genpath("/home/user/Documents/radarConfChallenge/Phaser-Control-with-MATLAB/"))
